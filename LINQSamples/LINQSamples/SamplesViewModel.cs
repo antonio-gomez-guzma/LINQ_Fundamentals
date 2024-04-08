0735ef9dc758d567d1d10fdcad80c1f8ed9c9e83
AVANADE-CORP\antonio.gomez.guzman <antonio.gomez.guzman@avanade.com>
2024-04-08 12:41:46 +0200

    Demo final modulo 12

--- LINQSamples/LINQSamples/SamplesViewModel.cs
+++ LINQSamples/LINQSamples/SamplesViewModel.cs
@@ -8,11 +8,14 @@
     /// </summary>
     public List<IGrouping<string, Product>> GroupByQuery()
     {
-      List<IGrouping<string, Product>> list = null;
+      List<IGrouping<string, Product>> list;
       // Load all Product Data
       List<Product> products = ProductRepository.GetAll();
 
       // Write Query Syntax Here
+      list = (from prod in products
+              orderby prod.Size
+              group prod by prod.Size).ToList();
 
       return list;
     }
@@ -24,29 +27,12 @@
     /// </summary>
     public List<IGrouping<string, Product>> GroupByMethod()
     {
-      List<IGrouping<string, Product>> list = null;
+      List<IGrouping<string, Product>> list;
       // Load all Product Data
       List<Product> products = ProductRepository.GetAll();
 
       // Write Method Syntax Here
-      
-
-      return list;
-    }
-    #endregion
-
-    #region GroupByIntoQuery
-    /// <summary>
-    /// Group products by Size property. 'into' is optional.
-    /// </summary>
-    public List<IGrouping<string, Product>> GroupByIntoQuery()
-    {
-      List<IGrouping<string, Product>> list = null;
-      // Load all Product Data
-      List<Product> products = ProductRepository.GetAll();
-
-      // Write Query Syntax Here
-      
+      list = products.OrderBy(p => p.Size).GroupBy(p => p.Size).ToList();
 
       return list;
     }
@@ -58,12 +44,15 @@
     /// </summary>
     public List<IGrouping<string, Product>> GroupByUsingKeyQuery()
     {
-      List<IGrouping<string, Product>> list = null;
+      List<IGrouping<string, Product>> list;
       // Load all Product Data
       List<Product> products = ProductRepository.GetAll();
 
       // Write Query Syntax Here
-      
+      list = (from prod in products
+              group prod by prod.Size into sizes
+              orderby sizes.Key
+              select sizes).ToList();
 
       return list;
     }
@@ -75,12 +64,14 @@
     /// </summary>
     public List<IGrouping<string, Product>> GroupByUsingKeyMethod()
     {
-      List<IGrouping<string, Product>> list = null;
+      List<IGrouping<string, Product>> list;
       // Load all Product Data
       List<Product> products = ProductRepository.GetAll();
 
       // Write Method Syntax Here
-     
+      list = products.GroupBy(prod => prod.Size)
+                     .OrderBy(sizes => sizes.Key)
+                     .Select(sizes => sizes).ToList();
 
       return list;
     }
@@ -93,12 +84,16 @@
     /// </summary>
     public List<IGrouping<string, Product>> GroupByWhereQuery()
     {
-      List<IGrouping<string, Product>> list = null;
+      List<IGrouping<string, Product>> list;
       // Load all Product Data
       List<Product> products = ProductRepository.GetAll();
 
       // Write Query Syntax Here
-      
+      list = (from prod in products
+              orderby prod.Size
+              group prod by prod.Size into sizes
+              where sizes.Count() > 2
+              select sizes).ToList();
 
       return list;
     }
@@ -111,12 +106,15 @@
     /// </summary>
     public List<IGrouping<string, Product>> GroupByWhereMethod()
     {
-      List<IGrouping<string, Product>> list = null;
+      List<IGrouping<string, Product>> list;
       // Load all Product Data
       List<Product> products = ProductRepository.GetAll();
 
       // Write Method Syntax Here
-      
+      list = products.OrderBy(p => p.Size)
+                     .GroupBy(prod => prod.Size)
+                     .Where(sizes => sizes.Count() > 2)
+                     .Select(sizes => sizes).ToList();
 
       return list;
     }
@@ -128,14 +126,25 @@
     /// </summary>
     public List<SaleProducts> GroupBySubQueryQuery()
     {
-      List<SaleProducts> list = null;
+      List<SaleProducts> list;
       // Load all Product Data
       List<Product> products = ProductRepository.GetAll();
       // Load all Sales Data
       List<SalesOrder> sales = SalesOrderRepository.GetAll();
 
       // Write Query Syntax Here
-      
+      list = (from sale in sales
+              orderby sale.SalesOrderID
+              group sale by sale.SalesOrderID into newSales
+              select new SaleProducts
+              {
+                SalesOrderID = newSales.Key,
+                Products = (from prod in products
+                            orderby prod.ProductID
+                            join sale in sales on prod.ProductID equals sale.ProductID
+                            where sale.SalesOrderID == newSales.Key
+                            select prod).ToList()
+              }).ToList();
 
       return list;
     }
@@ -147,14 +156,23 @@
     /// </summary>
     public List<SaleProducts> GroupBySubQueryMethod()
     {
-      List<SaleProducts> list = null;
+      List<SaleProducts> list;
       // Load all Product Data
       List<Product> products = ProductRepository.GetAll();
       // Load all Sales Data
       List<SalesOrder> sales = SalesOrderRepository.GetAll();
 
       // Write Method Syntax Here
-      
+      list = sales.OrderBy(s => s.SalesOrderID)
+                  .GroupBy(sale => sale.SalesOrderID)
+                  .Select(newSales => new SaleProducts
+                  {
+                    SalesOrderID = newSales.Key,
+                    Products = products.OrderBy(p => p.ProductID)
+                      .Join(newSales, prod => prod.ProductID,
+                            sale => sale.ProductID,
+                            (prod, sale) => prod).ToList()
+                  }).ToList();
 
       return list;
     }
@@ -167,12 +185,16 @@
     /// </summary>
     public List<string> GroupByDistinctQuery()
     {
-      List<string> list = null;
+      List<string> list;
       // Load all Product Data
       List<Product> products = ProductRepository.GetAll();
 
       // Write Query Syntax Here
-      
+      list = (from prod in products
+              orderby prod.Color
+              group prod by prod.Color into groupedColors
+              select groupedColors.FirstOrDefault().Color).ToList();
+
       return list;
     }
     #endregion
@@ -184,12 +206,14 @@
     /// </summary>
     public List<string> GroupByDistinctMethod()
     {
-      List<string> list =null;
+      List<string> list;
       // Load all Product Data
       List<Product> products = ProductRepository.GetAll();
 
       // Write Method Syntax Here
-      
+      list = products.GroupBy(p => p.Color)
+                      .Select(groupedColors => groupedColors.FirstOrDefault().Color)
+                      .OrderBy(c => c).ToList();
 
       return list;
     }
